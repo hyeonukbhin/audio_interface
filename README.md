588908c191b76d35d620905942e98e561b809e49
hyeonukbhin <bhu@kist.re.kr>
2018-04-18 09:17:20 +0900

    초기 구조 설정

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# sound_interface
+sHRI-Lab : Audio Interface for DTC Project
+===========================================================
+
+ROS 기반 Audio 입출력 Interface
+
+## 1. Description
+
+
+### 1.1 Framework Structure
+
+![H/W Structure](./assets/images/audio_interface.png)
+
+### 2.1 Requirements
+
+-	Ubuntu 14.04 or ~~Ubuntu 16.04~~
+-	[ROS](http://wiki.ros.org/) - Robot Operating System.
+-	Many python package(requirements.txt에 기재)
+```
+sudo pip install -r requirements.txt
+```
+
+## 3. Usage
+--------
+
+```
+roslaunch bringup_audio_interface bringup_audio_interface.launch
+```
---
+++ audio_streamer/script/audio_stream_from_mic.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pyaudio
+import rospy
+from audio_msgs.msg import AudioData
+
+
+def main():
+    rospy.init_node('audio_streamer')
+    scan_pub = rospy.Publisher('audio_stream', AudioData, queue_size=50)
+
+    ##### Initial Setting #####
+    FORMAT = pyaudio.paInt16
+
+    if rospy.has_param('~DEVICE_INDEX'):
+        INPUT_DEVICE = rospy.get_param('~DEVICE_INDEX')
+        CHANNELS = rospy.get_param('~CHANNELS')
+        RATE = rospy.get_param('~RATE')
+        LOOP_RATE = rospy.get_param('~LOOP_RATE')
+        CHUNK = int(RATE / LOOP_RATE)  # 100ms
+
+    else:
+        INPUT_DEVICE = 7
+        CHANNELS = 1
+        RATE = 44100
+        LOOP_RATE = 20
+        CHUNK = int(RATE / LOOP_RATE)  # 100ms
+
+    audio_interface = pyaudio.PyAudio()
+    r = rospy.Rate(LOOP_RATE)
+
+    stream = audio_interface.open(format=FORMAT,
+                                  input_device_index=INPUT_DEVICE,
+                                  channels=CHANNELS,
+                                  rate=RATE,
+                                  input=True,
+                                  output=True,
+                                  frames_per_buffer=CHUNK)
+
+    # INPUT_DEVICE_INFO = audio_interface.get_default_input_device_info()
+    INPUT_DEVICE_INFO = audio_interface.get_device_info_by_index(INPUT_DEVICE)
+    print INPUT_DEVICE_INFO
+
+    count = 0
+    while not rospy.is_shutdown():
+
+        current_time2 = rospy.get_rostime()
+        if count >= 10:
+            rospy.loginfo("Current time %i %i", current_time2.secs, current_time2.nsecs)
+            count = 0
+        else:
+            count += 1
+
+        try:
+            byte_buff = stream.read(CHUNK)
+            int16_buff = map(ord, byte_buff)
+            scan_pub.publish(int16_buff)
+        # print len(int16_buff)
+        except IOError, e:
+            if e[1] == pyaudio.paInputOverflowed:
+                print e
+                # x = '\x00' * 16 * 256 * 2  # value*format*chunk*nb_channels
+
+        r.sleep()
+
+if __name__ == '__main__':
+    main()
