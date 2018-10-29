78f14e6aebb7625b674fe7140b75480a082d06d1
hyeonukbhin <bhu@kist.re.kr>
2018-10-29 16:03:40 +0900

    중간 update

--- audio_signal_processor/script/audio_signal_processor.py
+++ audio_signal_processor/script/audio_signal_processor.py
@@ -1,74 +1,112 @@
-#!/usr/bin/python
+#!/usr/bin/python2
 # -*- coding: utf-8 -*-
 from __future__ import division
-import rospy
-from std_msgs.msg import String
-from audio_msgs.msg import AudioData
-# import pyaudio
-from six.moves import queue
-import os
-import sounddevice as sd
-import parselmouth
+
 import numpy as np
-import pandas as pd
-# import parselmouth
-# buff = []
+import parselmouth
+import rospy
+from audio_msgs.msg import AudioData
+from six.moves import queue
+from std_msgs.msg import String
+import sounddevice as sd
+
+wav_array = np.array([])
+count = 0
+
+time_step = 0.1
+pitch_floor = 75.0
+max_number_of_candidates = 15
+very_accurate = True
+silence_threshold = 0.03
+voicing_threshold = 0.45
+octave_cost = 0.01
+octave_jump_cost = 0.35
+voiced_unvoiced_cost = 0.14
+ceiling = 600.0
 
 
 def main():
+    global pub
     buff = queue.Queue()
     rospy.init_node('audio_signal_processor', anonymous=None)
-    global pub
     pub = rospy.Publisher('sound_information', String, queue_size=100)
-    rospy.Subscriber("audio_stream", AudioData, packetCallback, buff)
+    rospy.Subscriber("audio_stream", AudioData, callback_packet, buff)
     rospy.spin()
 
 
-def packetCallback(packetData, buff):
-    global callerSpeech, idx, callerArray, frameIdx, endFlag, lastFrameIdx
+def callback_packet(packetData, buff):
+    global callerSpeech, idx, callerArray, frameIdx, endFlag, lastFrameIdx, count, wav_array
     callerSpeech = packetData.data
-    byte_str = makeByteStr(callerSpeech)
-    # print(byte_str)
+    byte_str = make_byte_str(callerSpeech)
     buff.put(byte_str)
-    print([buff.get()])
+    wav_r = np.fromstring(buff.get(), dtype=np.int16).astype(np.float64) / 32768
+
+    if count >= 20:
+        amplitude = amp_calculation(wav_array)
+        pitch = pitch_calculation(wav_array)
+        intensity = intensity_calculation(wav_array)
+        print(amplitude)
+        print(len(amplitude))
+        play_audio(amplitude)
+        # print(pitch)
+        # print(len(pitch))
+        # print(intensity)
+        # print(len(intensity))
+        count = 0
+        wav_array = np.array([])
+    else:
+        wav_array = np.append(wav_array, wav_r)
+        count += 1
 
 
-    # list1 = [10, 1, 246, 0, 235, 0, 6, 1, 32, 1, 36, 1, 39, 1, 47, 1, 20, 1, 5, 1]
-    # list2 = [235, 255, 57, 0, 153, 0, 219, 0, 3, 1, 221, 0, 154, 0, 157, 0, 210, 0, 35, 1]
-    # list3 = [105, 1, 112, 1, 126, 1, 123, 1, 64, 1, 250, 0, 199, 0, 178, 0, 162, 0, 114, 0]
-    # list4 = [46, 0, 36, 0, 46, 0, 44, 0, 36, 0, 50, 0, 23, 0, 250, 255, 213, 255, 218]
-    # snd = parselmouth.Sound("docs/examples/audio/the_north_wind_and_the_sun.wav")
-    # snd = parselmouth.Sound()
-    #
-    #
-    #
-    # 1.
-    # __init__(self: parselmouth.Sound, values: numpy.ndarray[float64], sampling_frequency: Positive[
-    #     float] = 44100.0, start_time: float = 0.0) -> None
-
-    # plt.figure()
-    # plt.plot(snd.xs(), snd.values.T)
-    # plt.xlim([snd.xmin, snd.xmax])
-    # plt.xlabel("time [s]")
-    # plt.ylabel("amplitude")
-    # plt.show()
-
-
-    # 1.
-    # _(self: parselmouth.Sound, values: numpy.ndarray[float64], sampling_frequency: Positive[
-    #     float] = 44100.0, start_time: float = 0.0) -> None
-
-
-#    callerSpeech = packetData.data
-#    byte_str = makeByteStr(callerSpeech)
-#    buff.put(byte_str)
-
-def makeByteStr(int16Array):
-
+def make_byte_str(int16Array):
     byte_str = "".join(map(chr, int16Array))
     return byte_str
+
+def amp_calculation(wav_array):
+    snd = parselmouth.Sound(wav_array)
+    return snd.values.T
+
+def pitch_calculation(wav_array):
+    snd = parselmouth.Sound(wav_array)
+    pitch = snd.to_pitch_ac(time_step=time_step,
+                            pitch_floor=pitch_floor,
+                            max_number_of_candidates=max_number_of_candidates,
+                            very_accurate=very_accurate,
+                            silence_threshold=silence_threshold,
+                            voicing_threshold=voicing_threshold,
+                            octave_cost=octave_cost,
+                            octave_jump_cost=octave_jump_cost,
+                            voiced_unvoiced_cost=voiced_unvoiced_cost,
+                            pitch_ceiling=ceiling
+                            )
+    pitch_valuses = np.array(pitch.selected_array['frequency'])
+    pitch_xs = np.round(np.array(pitch.xs()), 4)
+
+    return pitch_valuses
+
+
+def intensity_calculation(wav_array):
+    snd = parselmouth.Sound(wav_array)
+    intensity = snd.to_intensity(minimum_pitch=pitch_floor,
+                                 time_step=time_step,
+                                 subtract_mean=True)
+
+    intensity_values_valuses = np.array(intensity.values.T)
+    intensity_xs = np.round(np.array(intensity.xs()), 4)
+
+    return intensity_values_valuses
+
+
+def interval_calculation(wav_array):
+    snd = parselmouth.Sound(wav_array)
+    return ""
+
+def play_audio(myarray):
+    sd.default.samplerate = 44100
+    # sd.default.device =
+    sd.play(myarray)
 
 
 if __name__ == '__main__':
     main()
-
--- audio_streamer/script/audio_stream_from_mic.py
+++ audio_streamer/script/audio_stream_from_mic.py
@@ -12,33 +12,40 @@ def main():
 
     ##### Initial Setting #####
     FORMAT = pyaudio.paInt16
+    audio_interface = pyaudio.PyAudio()
+    info = audio_interface.get_host_api_info_by_index(0)
+    numdevices = info.get('deviceCount')
+
+    device_index = 0
+    device_name = "Not Selected"
+    for i in range(0, numdevices):
+        if (audio_interface.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+            name = audio_interface.get_device_info_by_host_api_device_index(0, i).get('name')
+            print "Input Device id ", i, " - ", name
+            if "USB Audio Device" in name:
+                device_index = i
+                device_name = name
 
     if rospy.has_param('~DEVICE_INDEX'):
         INPUT_DEVICE = rospy.get_param('~DEVICE_INDEX')
         CHANNELS = rospy.get_param('~CHANNELS')
         RATE = rospy.get_param('~RATE')
         LOOP_RATE = rospy.get_param('~LOOP_RATE')
-        CHUNK = int(RATE / LOOP_RATE)  # 100ms
+        CHUNK = rospy.get_param('~CHUNK')
+        # CHUNK = int(RATE / LOOP_RATE)  # 100ms
 
     else:
-        INPUT_DEVICE = 5
+        INPUT_DEVICE = device_index
+        print("\x1b[1;33m[Device Information] : {}\x1b[1;m".format(device_name))
         CHANNELS = 1
         RATE = 44100
         LOOP_RATE = 10
-        CHUNK = int(RATE / LOOP_RATE)  # 100ms
+        CHUNK = 8192  # 100ms
+        # CHUNK = 4000  # 100ms
 
-    audio_interface = pyaudio.PyAudio()
+    print(CHUNK)
     r = rospy.Rate(LOOP_RATE)
-
-    stream = audio_interface.open(format=FORMAT,
-                                  input_device_index=INPUT_DEVICE,
-                                  channels=CHANNELS,
-                                  rate=RATE,
-                                  input=True,
-                                  output=True,
-                                  frames_per_buffer=CHUNK)
-
-    # INPUT_DEVICE_INFO = audio_interface.get_default_input_device_info()
+    stream = make_stream(audio_interface, FORMAT, INPUT_DEVICE, CHANNELS, RATE, CHUNK)
     INPUT_DEVICE_INFO = audio_interface.get_device_info_by_index(INPUT_DEVICE)
     print INPUT_DEVICE_INFO
 
@@ -46,7 +53,7 @@ def main():
     while not rospy.is_shutdown():
 
         current_time2 = rospy.get_rostime()
-        if count >= 10:
+        if count >= LOOP_RATE:
             rospy.loginfo("Current time %i %i", current_time2.secs, current_time2.nsecs)
             count = 0
         else:
@@ -54,19 +61,30 @@ def main():
 
         try:
             byte_buff = stream.read(CHUNK)
-            print(byte_buff)
             int16_buff = map(ord, byte_buff)
             scan_pub.publish(int16_buff)
-            # print type(int16_buff)
-            # print int16_buff
-            print int16_buff[:20]
-            # print len(int16_buff)
+            print(byte_buff[:20])
+            print(int16_buff[:20])
         except IOError, e:
-            if e[1] == pyaudio.paInputOverflowed:
-                print e
-                # x = '\x00' * 16 * 256 * 2  # value*format*chunk*nb_channels
+            print("\x1b[1;31m[Error Massage] : %s\x1b[1;m" % e)
+            stream.close()
+            stream = make_stream(audio_interface, FORMAT, INPUT_DEVICE, CHANNELS, RATE, CHUNK)
 
         r.sleep()
+
+
+def make_stream(audio_interface, format, input_device_index, channels, rate, frames_per_buffer, input=True, output=True):
+    stream = audio_interface.open(format=format,
+                                  input_device_index=input_device_index,
+                                  channels=channels,
+                                  frames_per_buffer=frames_per_buffer,
+                                  rate=rate,
+                                  input=input,
+                                  output=output
+                                  )
+
+    return stream
+
 
 if __name__ == '__main__':
     main()
